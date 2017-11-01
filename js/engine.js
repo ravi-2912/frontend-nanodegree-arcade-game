fe0ed452d1c7a93f08f704e23d58dc43c24eb929
Ravinder Singh <ravi_29_12@hotmail.com>
2017-11-01 23:53:45 +0000

    Rendering collectbles above player.

--- js/engine.js
+++ js/engine.js
@@ -95,8 +95,9 @@ var Engine = (function(global) {
         });
         
         timeText.update(dt);
-        collectibles.update(dt);
         player.update(dt);
+        collectibles.update(dt);
+        
     }
 
     /* This checks collision between players and enemies and collectibles,
@@ -169,8 +170,9 @@ var Engine = (function(global) {
             enemy.render();
         });
 
-        collectibles.render();
         player.render();
+        collectibles.render();
+        
         timeText.render();
         
     }
@@ -193,6 +195,10 @@ var Engine = (function(global) {
         "images/grass-block.png",
         "images/enemy-bug.png",
         "images/char-boy.png",
+        "images/char-cat-girl.png",
+        "images/char-horn-girl.png",
+        "images/char-pink-girl.png",
+        "images/char-princess-girl.png",
         "images/Heart.png",
         "images/Star.png",
         "images/Gem Blue.png",
